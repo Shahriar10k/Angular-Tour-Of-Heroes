c6ee838c7d38f64a537f2091fb6f95da63c65c4a
Shahriar Khan <shahriar.khan@synesisit.info>
2022-09-25 17:56:23 +0600

    2nd

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,4 +17,12 @@ export class HeroService {
     this.messageService.add('HeroService: feched heroes');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero>{
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(
+      `HersoService: fetched hero id=${id}`
+    )
+    return of(hero);
+  }
 }
